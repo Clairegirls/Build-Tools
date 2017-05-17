2e7cf005bfa8f2bc8b0f6f4929f5886ad9ed167b
wangjianfei <444128852@qq.com>
2017-05-17 18:40:15 +0800

    add demo about webpack and handlebars.js

---
+++ demos/degula/src/js/entry.js
@@ -0,0 +1,7 @@
+import { app } from "./app"
+require("./flexible.js");
+// require("../styles/test.scss");
+require("../styles/base.css");
+require("../styles/container.css");
+require("./main");
+app.start();
